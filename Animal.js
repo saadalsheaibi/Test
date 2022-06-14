b02cd57dd3c1b4da27265911ebd2833a77ff083f
saadalsheaibi <dr.craazy@hotmail.com>
2022-06-14 21:14:33 +0300

    First task

---
+++ Animal.js
@@ -0,0 +1,10 @@
+export default class  Ani{
+        name;
+        type;
+
+        constructor (name,type){
+            this.name=name;
+            this.type=type;
+    
+        }    
+    } 
---
+++ app.js
@@ -0,0 +1,4 @@
+import Ani from "./Animal.js";
+
+let z = new Ani("saad","Meshari");
+console.log(z);
